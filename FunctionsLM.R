475f6f34dfc762cd77c5125316bfeaddb6df8ea3
Rachael Shudde <rachael.shudde@gmail.com>
2019-08-28 11:04:21 -0500

    Wrote functions calculateBeta and calculateMSE

--- FunctionsLM.R
+++ FunctionsLM.R
@@ -1,11 +1,13 @@
-# Generate n-dimensional response Y that follows linear regression model Y = Xbeta + epsilon, where epsilon is normal zero with variance sigma^2 independent across samples. Seed should be set at the beginning of the function
+# Generate n-dimensional response Y that follows linear regression model Y = Xbeta + epsilon, 
+# where epsilon is normal zero with variance sigma^2 independent across samples. 
+# Seed should be set at the beginning of the function
 # X - design matrix
 # beta - given parameter vector
 # sigma - standard deviation of the noise
 # seed  - starting seed value
 generateY <- function(X, beta, sigma, seed = 5832652){
   #[ToDo] Set seed and generate Y following linear model
-  
+  set.seed(seed)
   # Return Y
   return(Y)
 }
@@ -14,15 +16,16 @@ generateY <- function(X, beta, sigma, seed = 5832652){
 # X - design matrix
 # Y -response
 calculateBeta <- function(X, Y){
-  # Calculate beta_LS
-  
+  # Calculate beta_LS using the least squares solution b = (X^TX)^(-1)X^Ty
+  beta_LS = solve((t(X) %*% X)) %*% t(X) %*% y
   # Return beta
   return(beta_LS)
 }
 
 # Calculate MSE
 calculateMSE <- function(beta, beta_LS){
-  
-  # Return MSE - error ||beta - beta_LS||_2^2
+  # calculate error: ||beta - beta_LS||_2^2
+  MSE = abs(beta - beta_LS)^2
+  # Return MSE
   return(MSE)
 }
