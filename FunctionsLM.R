88d8bc2d2fbc3d5aaaf538412a145d65d2fa8261
Rachael Shudde <rachael.shudde@gmail.com>
2019-08-29 15:09:58 -0500

    Updated commits after going over R style guide in class

--- FunctionsLM.R
+++ FunctionsLM.R
@@ -6,25 +6,23 @@
 # sigma - standard deviation of the noise
 # seed  - starting seed value
 generateY <- function(X, beta, sigma, seed = 5832652){
-  #[ToDo] Set seed and generate Y following linear model
+  # set seed and size of Y = number of rows in X
   set.seed(seed)
-  n = nrow(X)
-  epsilon = rnorm(n, 0, sigma^2)
+  size_Y = nrow(X)
+  # generate random epsilon
+  epsilon = rnorm(size_Y, 0, sigma^2)
   
   # generate Y using epsilon above: Y = X*beta + epsilon
   Y = X%*%beta + epsilon
-  
-  # Return Y
-  return(Y)
+    return(Y)
 }
 
 # Calculate beta_LS - least-squares solution, do not use lm function
-# X - design matrix
+# X - design matrix (assume invertible matrix)
 # Y -response
 calculateBeta <- function(X, Y){
-  # Calculate beta_LS using the least squares solution b = (X^TX)^(-1)X^Ty
+  # calculate beta_LS using the least squares solution b = (X^TX)^(-1)X^Ty
   beta_LS = solve((t(X) %*% X)) %*% t(X) %*% y
-  # Return beta
   return(beta_LS)
 }
 
@@ -32,7 +30,6 @@ calculateBeta <- function(X, Y){
 calculateMSE <- function(beta, beta_LS){
   # calculate error: ||beta - beta_LS||_2^2
   MSE = sum((beta - beta_LS)^2) 
-  # Return MSE
   return(MSE)
 }
 
